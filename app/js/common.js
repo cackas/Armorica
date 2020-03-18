7b8fb57fbb55ac1aca4264a05b9de61a08c3b5e0
cackas <vannigga@yandex.ru>
2020-03-18 23:44:47 +0300

    popups rebuild

--- app/js/common.js
+++ app/js/common.js
@@ -40,6 +40,7 @@ $(function() {
 			$('#menu-popup').removeClass('open');
 			$('#affiliate-popup').removeClass('open');
 			$('#advertiser-popup').removeClass('open');
+			$('.landing').removeClass('hidden');
 		})
 
 	}
@@ -50,6 +51,7 @@ $(function() {
 			e.preventDefault();
 			$('.header__top').addClass('open');
 			$('#menu-popup').addClass('open');
+			$('.landing').addClass('hidden');
 		})
 
 	}
@@ -60,6 +62,7 @@ $(function() {
 			e.preventDefault();
 			$('.header__top').addClass('open');
 			$('#affiliate-popup').addClass('open');
+			$('.landing').addClass('hidden');
 		})
 
 	}
@@ -70,6 +73,7 @@ $(function() {
 			e.preventDefault();
 			$('.header__top').addClass('open');
 			$('#advertiser-popup').addClass('open');
+			$('.landing').addClass('hidden');
 		})
 
 	}
@@ -79,41 +83,48 @@ $(function() {
 		$('.signin-btn').click(function(e){
 			e.preventDefault();
 			$('.log-in-wrap').addClass('open');
+			$('body').addClass('overflow-stop');
 		})
 
 		$('#forget-btn').click(function(e){
 			e.preventDefault();
 			$('.forget-pass').addClass('open');
 			$('.log-in').removeClass('open');
+			$('body').addClass('overflow-stop');
 		})
 		
 		$('#pretitle--forget-pass').click(function(e){
 			e.preventDefault();
 			$('.log-in').addClass('open');
 			$('.forget-pass').removeClass('open');
+			$('body').addClass('overflow-stop');
 		})
 		
 		$('#pretitle--change-pass').click(function(e){
 			e.preventDefault();
 			$('.log-in').addClass('open');
 			$('.change-pass').removeClass('open');
+			$('body').addClass('overflow-stop');
 		})
 		
 		$('#email-sent-btn,#email-sent-ok-btn').click(function(e){
 			e.preventDefault();
 			$('.log-in').addClass('open');
 			$('.email-sent').removeClass('open');
+			$('body').addClass('overflow-stop');
 		})
 		
 		$('#pass-changed-btn').click(function(e){
 			e.preventDefault();
 			$('.log-in').addClass('open');
 			$('.pass-changed').removeClass('open');
+			$('body').addClass('overflow-stop');
 		})
 
 		$('.close--log-in').click(function(e){
 			e.preventDefault();
 			$('.log-in-wrap').removeClass('open');
+			$('body').removeClass('overflow-stop');
 		})
 		
 	}
